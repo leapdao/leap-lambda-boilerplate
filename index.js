df2b7cec9ad426158116d8af4282f370fcd70b09
Kosta Korenkov <kosta@whoot.me>
2019-04-09 16:32:10 +0300

    fix(setup): set index.js for package

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright (c) 2018-present, LeapDAO (leapdao.org)
- *
- * This source code is licensed under the Mozilla Public License Version 2.0
- * found in the LICENSE file in the root directory of this source tree.
- */
-
-console.log('put your code here');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Db = require('./db');
+const Errors = require('./errors');
+const Queue = require('./queue');
+
+module.exports = { Db, Errors, Queue };
